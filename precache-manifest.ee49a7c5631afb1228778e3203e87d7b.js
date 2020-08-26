44ba337727dd0b892eff5d3522076f4c07741c85
Mel <mel.ndrw@gmail.com>
2020-08-26 19:20:08 +0800

    Updates

---
+++ precache-manifest.ee49a7c5631afb1228778e3203e87d7b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd44c1ad928fcd893d1549dcad2c4382",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "f85ab3cc6e61f4a70bb7",
+    "url": "/react-todolist/static/css/2.a16e1df3.chunk.css"
+  },
+  {
+    "revision": "d651ccd439d172cfdcc5",
+    "url": "/react-todolist/static/css/main.13e892a1.chunk.css"
+  },
+  {
+    "revision": "f85ab3cc6e61f4a70bb7",
+    "url": "/react-todolist/static/js/2.57469a9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.57469a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d651ccd439d172cfdcc5",
+    "url": "/react-todolist/static/js/main.5dade51d.chunk.js"
+  },
+  {
+    "revision": "0acd6faf322e1d53f473",
+    "url": "/react-todolist/static/js/runtime-main.c17b1b81.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/react-todolist/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/react-todolist/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/react-todolist/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/react-todolist/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/react-todolist/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/react-todolist/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/react-todolist/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/react-todolist/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/react-todolist/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/react-todolist/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/react-todolist/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/react-todolist/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/react-todolist/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/react-todolist/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/react-todolist/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
